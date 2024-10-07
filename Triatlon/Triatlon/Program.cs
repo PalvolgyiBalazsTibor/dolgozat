88a3a1bcbb8b5079ec7f392bd2fc800e6d453c1e
palvolgyi.balazs <palvolgyi.balazs@28-DIAK15>
2024-10-07 12:37:29 +0200

    Add project files.

---
+++ Triatlon/Triatlon/Program.cs
@@ -0,0 +1,41 @@
+﻿using Triatlon;
+
+List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+using StreamReader sr = new("forras.txt"); 
+
+while (!sr.EndOfStream)
+{
+    string[] sor = sr.ReadLine().Split(";");
+
+    Versenyzo versenyzo = new Versenyzo();
+
+    versenyzo.nev = sor[0];
+    versenyzo.szulev = int.Parse(sor[1]);
+    versenyzo.rajtszam = int.Parse(sor[2]);
+    versenyzo.nem = sor[3];
+    versenyzo.csoportositas = sor[4];
+    versenyzo.uszasideje = TimeSpan.Parse(sor[5]);
+    versenyzo.elsodepo = TimeSpan.Parse(sor[6]);
+    versenyzo.kerekparideje = TimeSpan.Parse(sor[7]);
+    versenyzo.masodikdepo = TimeSpan.Parse(sor[8]);
+    versenyzo.futasideje = TimeSpan.Parse(sor[9]);
+
+    versenyzok.Add(versenyzo);
+}
+sr.Close();
+
+//versenyzők száma 'elit junior' kategóriában
+Console.WriteLine($"Versenyzők száma: {versenyzok.Count} darab");
+
+//férfi versenyzők átlagéletkora
+double ferfiatlagkor = versenyzok.Where(a => a.nem == "f").Average(a => a.kor);
+Console.WriteLine($"A férfi versenyzők átlag átlagéletkora: {ferfiatlagkor:0.00} év");
+
+//a versenyzők futással töltött összideje órában. 2tj-ig kerekítve
+double futassaltoltottido = versenyzok.Sum(b => b.futasideje.TotalHours);
+Console.WriteLine($"A versenyzők futással töltött összideje órában: {futassaltoltottido:0.00} óra");
+
+//átlagos úszási idő 20-24 kategóriában
+double atlaguszas2024 = versenyzok.Where(c => c.csoportositas == "20-24").Average(c => c.uszasideje.TotalMinutes);
+Console.WriteLine($"Az átlagos úszási idő 20-24 kategóriában: {atlaguszas2024:0.00} perc");
---
+++ Triatlon/Triatlon/Versenyzo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triatlon
+{
+    internal class Versenyzo
+    {
+        public string? nev { get; set; }
+        public int szulev { get; set; }
+        public int kor => DateTime.Now.Year - szulev;
+        public int rajtszam { get; set; }
+
+        //public bool nem { get; set; }
+        public string? nem { get; set; }
+
+        public string? csoportositas { get; set; }
+        public TimeSpan uszasideje { get; set; }
+        public TimeSpan elsodepo { get; set; }
+        public TimeSpan kerekparideje { get; set; }
+        public TimeSpan masodikdepo { get; set; }
+        public TimeSpan futasideje { get; set; }
+        public TimeSpan osszido => uszasideje + elsodepo + kerekparideje + masodikdepo + futasideje;
+
+        public override string ToString()
+        {
+         return $"Versenyző neve: {nev}\n" +
+                $"Születési éve: {szulev}\n" +
+                $"Rajtszáma: {rajtszam}\n" +
+                $"Neme: {/*(nem ? "férfi" : "nő")*/ nem}\n" +
+                $"Kategóriája: {csoportositas}\n" +
+                $"Időeredmények:" + $"\n" +
+                $"Úszás ideje: {uszasideje}\n" +
+                $"Első depóban töltött idő: {elsodepo}\n" +
+                $"Kerékpározás ideje: {kerekparideje}" + $"\n" +
+                $"Második depóban töltött idő: {masodikdepo}\n" +
+                $"Futás ideje: {futasideje}";
+        }
+    }
+}
